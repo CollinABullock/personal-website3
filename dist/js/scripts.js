f59d4a6ef50d5089b3e028dca1cfca647627ffa3
Collin Bullock <cbullock@DESKTOP-TFGR83M>
2023-12-18 15:48:00 -0600

    trying to sync between github and netlify

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,7 +1,23 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+// Function to be called when the element enters the viewport
+function slideIn(entries, observer) {
+  entries.forEach(entry => {
+      if (entry.isIntersecting) {
+          entry.target.classList.add('slide'); // Add a CSS class for sliding animation
+          observer.unobserve(entry.target); // Stop observing once animation is triggered
+      }
+  });
+}
+
+// Using Intersection Observer to detect when the element enters the viewport
+const slideInOptions = {
+  root: null,
+  threshold: 0.2 // Adjust this threshold as needed
+};
+
+const slideInObserver = new IntersectionObserver(slideIn, slideInOptions);
+const slideInElement = document.getElementById('slide-in');
+slideInObserver.observe(slideInElement);
+
+// Optionally, you can do the same for the skills list
+const skillsList = document.getElementById('skills-list');
+slideInObserver.observe(skillsList);
