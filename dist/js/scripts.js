739d7845bf0cd9e51b358e0ce66580a19c6a1718
Collin Bullock <cbullock@DESKTOP-TFGR83M>
2023-12-18 17:02:03 -0600

    added javascript animation

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,23 +1,7 @@
-// Function to be called when the element enters the viewport
-function slideIn(entries, observer) {
-  entries.forEach(entry => {
-      if (entry.isIntersecting) {
-          entry.target.classList.add('slide'); // Add a CSS class for sliding animation
-          observer.unobserve(entry.target); // Stop observing once animation is triggered
-      }
-  });
-}
+document.addEventListener("DOMContentLoaded", function() {
+  // Get the slide-in element
+  const slideInElement = document.querySelector('.slide-in');
 
-// Using Intersection Observer to detect when the element enters the viewport
-const slideInOptions = {
-  root: null,
-  threshold: 0.2 // Adjust this threshold as needed
-};
-
-const slideInObserver = new IntersectionObserver(slideIn, slideInOptions);
-const slideInElement = document.getElementById('slide-in');
-slideInObserver.observe(slideInElement);
-
-// Optionally, you can do the same for the skills list
-const skillsList = document.getElementById('skills-list');
-slideInObserver.observe(skillsList);
+  // Add the 'active' class to trigger the slide-in animation
+  slideInElement.classList.add('active');
+});
